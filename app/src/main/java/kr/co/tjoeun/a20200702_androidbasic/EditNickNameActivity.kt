28e5fa4afdbd53141106a9150f698b03d966e63c
채정실 <cowjdtlf@gmail.com>
2020-07-02 21:38:28 +0900

    닉네임 변경화면에서 입력값을 들고 복귀

--- app/src/main/java/kr/co/tjoeun/a20200702_androidbasic/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20200702_androidbasic/EditNickNameActivity.kt
@@ -1,11 +1,40 @@
 package kr.co.tjoeun.a20200702_androidbasic
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+
+//            뭐라고 닉네임을 바꾸는지 받아오자
+
+            val inputNick = nickNameEdt.text.toString()
+
+//            복귀용 티켓 Intent => 출발지/도착지 기재 x
+
+            val resultIntent = Intent()
+
+//            복귀용 Intent에 데이터 첨부
+
+            resultIntent.putExtra("nickName",inputNick)
+
+//            완료를 눌렀다는 것을 명시 + 결과용 Intent도 첨부
+
+            setResult(Activity.RESULT_OK,resultIntent)
+
+//            모든 처리가 완료되면 종료
+
+            finish()
+
+
+        }
+
     }
 }
